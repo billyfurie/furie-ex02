41bf96870c802bd3db88f03e2629b2196a301b4d
BillyFurie <81727683+BillyFurie@users.noreply.github.com>
2021-09-01 19:09:34 -0400

    Initial commit

---
+++ src/main/java/base/Solution02.java
@@ -0,0 +1,23 @@
+package base;
+
+import java.util.Scanner;
+
+public class Solution02 {
+
+    /*
+        print "What is the input string?"
+        `inputString` = read string from user
+        `numChars` = length of string
+        print `inputString` + " has " + `numChars` + " characters."
+     */
+
+    public static void main(String[] args) {
+        System.out.print("What is the input string? ");
+
+        Scanner input = new Scanner(System.in);
+        String inputString = input.nextLine();
+        int numChars = inputString.length();
+
+        System.out.println(inputString + " has " + numChars + " characters.");
+    }
+}
